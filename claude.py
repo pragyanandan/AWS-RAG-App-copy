27357aaf22b08065bd0aacafb1a8615c366ed729
pragyanandan <pragyanandan@gmail.com>
2024-10-13 13:53:48 +1300

    streamlit config changes

--- claude.py
+++ claude.py
@@ -1,21 +1,26 @@
 import boto3
 import json
 import os
+import streamlit as st
 
 prompt_data = """
 Act as a Shakespeare and write a poem on Generative AI
 """
 
-# Access credentials from environment variables
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+# Access AWS credentials from environment variables  [To RUN at LOCAL]
+#aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+#aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+# Access AWS credentials from environment variables  [To RUN at streamlit]
+aws_access_key_id = st.secrets["aws"]["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"]
+aws_region = st.secrets["aws"]["AWS_DEFAULT_REGION"]
 
 # Initialize the Bedrock client
 bedrock = boto3.client(
     service_name="bedrock-runtime",
     region_name="us-west-2",
     aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key"
+    aws_secret_access_key=aws_secret_access_key
 )
 
 # Prepare the payload with validated parameters
--- llama2.py
+++ llama2.py
@@ -1,20 +1,24 @@
 import boto3
 import json
-import os
+import streamlit as st
 
 prompt_data="""
 Act as a Shakespeare and write a poem on Genertaive AI
 """
 
-# Access credentials from environment variables
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+# Access AWS credentials from environment variables  [To RUN at LOCAL]
+#aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+#aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+# Access AWS credentials from environment variables  [To RUN at streamlit]
+aws_access_key_id = st.secrets["aws"]["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"]
+aws_region = st.secrets["aws"]["AWS_DEFAULT_REGION"]
 
 bedrock = boto3.client(
     service_name="bedrock-runtime",
     region_name="us-west-2",
     aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key"
+    aws_secret_access_key=aws_secret_access_key
 )
 
 payload={
